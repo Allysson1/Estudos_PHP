5493d8236faab4129cedce7b5aef19fb85459017
unknown <allyssonoliveira12345@gmail.com>
2025-03-27 21:12:56 -0300

    aula - estruturas de controle

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -16,12 +16,53 @@ fullStackPHPClassSession("if, elseif, else", __LINE__);
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+$rock = "";
+if(isset($rock)){ //isset siginifica "se exiteste a variável", mesmo que vazia, é válido, e entra no if
+    var_dump("JDM");
+    echo "<br>";
+};
 
+
+if(!isset($rock)){ //isset com ! siginifica "se NÃO exiteste a variável"
+    var_dump("JDM");
+    echo "<br>";
+}
+else{
+    var_dump("die");
+    echo "<br>";
+};
+
+$jdm = "toyota"; //empty = vazio, !empty = não vazio
+if (!empty($jdm)){
+    var_dump( "os melhores carros são os da {$jdm}");
+}
+else{
+    var_dump("O melhores são da nissan");
+};
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
  */
-fullStackPHPClassSession("switch", __LINE__);
+fullStackPHPClassSession("switch", __LINE__); //opção mais simples ao IF/ELSE
 
+$payment = "approved";
 
-
-
+switch ($payment){
+    case 'billet_printed':
+        var_dump("Boleto Impresso!");
+        break;
+    
+    case 'canceled':
+        var_dump("Pagamento Cancelado!");
+        break;
+    case 'past_due':
+    case 'pending':
+        var_dump("Aguardando Pagamento!");
+        break;
+    case 'approved':
+    case 'completed':
+        var_dump("Pagamento Aprovado!");
+        break;
+    default:
+    var_dump("erro ao processar pagamento!");
+    break;
+}
